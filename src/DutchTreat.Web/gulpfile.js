3c7af2e240d2eb3f658a6de6987d8ed6ce47fd49
SoundWavesStation <colladosmcloovin21@gmail.com>
2018-05-25 05:46:33 +0800

    Set up a gulpfile and bundleconfig

---
+++ src/DutchTreat.Web/gulpfile.js
@@ -0,0 +1,105 @@
+﻿
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rimraf = require("rimraf");
+var cssmin = require("gulp-cssmin");
+var htmlmin = require("gulp-htmlmin");
+var merge = require('merge-stream'); 
+var bundleconfig = require("./bundleconfig.json");
+var del = require("del");
+
+// Dependency Dirs
+var deps = {
+    "jquery": {
+        "dist/*": ""
+    },
+    "bootstrap": {
+        "dist/**/*": ""
+    },
+};
+var regex = {
+    css: /\.css$/,
+    html: /\.(html|htm)$/,
+    js: /\.js$/
+};
+
+gulp.task("clean", function () {
+
+    return del(["wwwroot/lib/", "wwwroot/minified/css/","wwwroot/minified/js/"]);
+});
+
+gulp.task("CopyToLibs", ['clean'], function () {
+    var streams = [];
+    for (var prop in deps) {
+        console.log("Prepping Scripts for: " + prop);
+        for (var itemProp in deps[prop]) {
+            streams.push(gulp.src("node_modules/" + prop + "/" + itemProp)
+                .pipe(gulp.dest("wwwroot/lib/" + prop + "/" + deps[prop][itemProp])));
+        }
+    }
+
+    return merge(streams);
+});
+
+gulp.task("min:js", function () {
+    var tasks = getBundles(regex.js).map(function (bundle) {
+        return gulp.src(bundle.inputFiles, { base: "." })
+            .pipe(concat(bundle.outputFileName))
+            .pipe(uglify())
+            .pipe(gulp.dest("."));
+    });
+    return merge(tasks);
+});
+
+gulp.task("min:css", function () {
+    var tasks = getBundles(regex.css).map(function (bundle) {
+        return gulp.src(bundle.inputFiles, { base: "." })
+            .pipe(concat(bundle.outputFileName))
+            .pipe(cssmin())
+            .pipe(gulp.dest("."));
+    });
+    return merge(tasks);
+});
+
+gulp.task("min:html", function () {
+    var tasks = getBundles(regex.html).map(function (bundle) {
+        return gulp.src(bundle.inputFiles, { base: "." })
+            .pipe(concat(bundle.outputFileName))
+            .pipe(htmlmin({ collapseWhitespace: true, minifyCSS: true, minifyJS: true }))
+            .pipe(gulp.dest("."));
+    });
+    return merge(tasks);
+});
+
+gulp.task("watch", function () {
+    getBundles(regex.js).forEach(function (bundle) {
+        gulp.watch(bundle.inputFiles, ["min:js"]);
+    });
+
+    getBundles(regex.css).forEach(function (bundle) {
+        gulp.watch(bundle.inputFiles, ["min:css"]);
+    });
+
+    getBundles(regex.html).forEach(function (bundle) {
+        gulp.watch(bundle.inputFiles, ["min:html"]);
+    });
+});
+
+function getBundles(regexPattern) {
+    return bundleconfig.filter(function (bundle) {
+        return regexPattern.test(bundle.outputFileName);
+    });
+}
+gulp.task("cleanmin", function () {
+    var files = bundleconfig.map(function (bundle) {
+        return bundle.outputFileName;
+    });
+
+    return del(files);
+});
+
+gulp.task("run", ['CopyToLibs']);
+gulp.task("start", ['cleanmin','min:js', 'min:css', 'min:html']);
+
+//'min:js', 'min:css', 'min:html'
--- src/DutchTreat.Web/wwwroot/js/index.js
+++ src/DutchTreat.Web/wwwroot/js/index.js
@@ -3,13 +3,17 @@ var s = "";
 
 console.log("Hello World! Console Log!");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", function() {
+
+var theForm = $("#theForm");
+theForm.hide();
+
+var button = $("#buyButton");
+button.on("click", function () {
     console.log("Buying Item");
 });
 
-var productInfo = document.getElementsByClassName("product-props");
-var listItems = productInfo.item[0].children;
+var productInfo = $(".product-props li");
+productInfo.on("click", function () {
+    console.log("You Clicked on" + $(this).text());
+});
